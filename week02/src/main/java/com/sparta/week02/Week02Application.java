f0abdbc9d330c38b0b44b27914f0eda91f15e135
swcide <swcide@gmail.com>
2021-06-26 12:08:14 +0900

    'test'

--- week02/src/main/java/com/sparta/week02/Week02Application.java
+++ week02/src/main/java/com/sparta/week02/Week02Application.java
@@ -1,13 +1,42 @@
 package com.sparta.week02;
 
+import com.sparta.week02.domain.Course;
+import com.sparta.week02.domain.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.List;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class Week02Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Week02Application.class, args);
     }
 
+    // Week02Application.java 의 main 함수 아래에 붙여주세요.
+    @Bean
+    public CommandLineRunner demo(CourseRepository repository) {
+        return (args) -> {
+            // 데이터 저장하기
+            repository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
+
+            // 데이터 전부 조회하기
+            List<Course> courseList = repository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+            // 데이터 하나 조회하기
+            Course course = repository.findById(1L).orElseThrow(
+                    () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
+            );
+        };
+    }
 }
---
+++ week02/src/main/java/com/sparta/week02/domain/test.java
@@ -0,0 +1,26 @@
+package com.sparta.week02.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+
+
+
+    public static void main(String[] args) {
+
+        List<String> fruits = new ArrayList<>();
+        fruits.add("감");
+        fruits.add("배");
+        fruits.add("감");
+
+        if(fruits.get(1) =="배"){
+            System.out.println("yes");
+        }else if (fruits == null) {
+            
+        }{
+            
+        }
+
+    }
+}

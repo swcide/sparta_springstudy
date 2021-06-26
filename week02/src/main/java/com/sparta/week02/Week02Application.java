649d68cdf649d659f295f86632d81aff9026fcd7
swcide <swcide@gmail.com>
2021-06-26 17:27:36 +0900

    'study'

--- week02/src/main/java/com/sparta/week02/Week02Application.java
+++ week02/src/main/java/com/sparta/week02/Week02Application.java
@@ -2,6 +2,8 @@ package com.sparta.week02;
 
 import com.sparta.week02.domain.Course;
 import com.sparta.week02.domain.CourseRepository;
+import com.sparta.week02.domain.CourseRequestDto;
+import com.sparta.week02.service.CourseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.List;
 
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class Week02Application {
@@ -17,26 +20,32 @@ public class Week02Application {
         SpringApplication.run(Week02Application.class, args);
     }
 
-    // Week02Application.java 의 main 함수 아래에 붙여주세요.
     @Bean
-    public CommandLineRunner demo(CourseRepository repository) {
+    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
         return (args) -> {
-            // 데이터 저장하기
-            repository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
+            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
 
-            // 데이터 전부 조회하기
-            List<Course> courseList = repository.findAll();
-            for (int i = 0; i < courseList.size(); i++) {
+            System.out.println("데이터 인쇄");
+            List<Course> courseList = courseRepository.findAll();
+            for (int i=0; i<courseList.size(); i++) {
                 Course course = courseList.get(i);
                 System.out.println(course.getId());
                 System.out.println(course.getTitle());
                 System.out.println(course.getTutor());
             }
 
-            // 데이터 하나 조회하기
-            Course course = repository.findById(1L).orElseThrow(
-                    () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
-            );
+
+            CourseRequestDto requestDto = new CourseRequestDto("웹개발의 봄, Spring", "임민영");
+            courseService.update(1L, requestDto);
+            courseList = courseRepository.findAll();
+            for (int i=0; i<courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+//            courseRepository.deleteAll();
         };
     }
-}
+};
--- week02/src/main/java/com/sparta/week02/domain/Course.java
+++ week02/src/main/java/com/sparta/week02/domain/Course.java
@@ -1,12 +1,14 @@
 package com.sparta.week02.domain;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Getter //겟터
 @NoArgsConstructor // 기본생성자를 대신 생성해줍니다.
 @Entity // 테이블임을 나타냅니다.
-public class Course extends Timestamped{
+public class Course extends Timestamped {
 
     @Id // ID 값, Primary Key로 사용하겠다는 뜻입니다.
     @GeneratedValue(strategy = GenerationType.AUTO) // 자동 증가 명령입니다.
@@ -18,16 +20,19 @@ public class Course extends Timestamped{
     @Column(nullable = false)
     private String tutor;
 
-    public String getTitle() {
-        return this.title;
-    }
-
-    public String getTutor() {
-        return this.tutor;
-    }
-
     public Course(String title, String tutor) {
         this.title = title;
         this.tutor = tutor;
     }
+    public Course(CourseRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.tutor = requestDto.getTutor();
+    }
+
+    public void update(CourseRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.tutor = requestDto.getTutor();
+    }
+
+
 }
